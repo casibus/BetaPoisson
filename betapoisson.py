c3dd0ac51498a1f4b7cdcd7ff3149be9ff11715f
Johannes Nagele <Johannes.Nagele@alexanderthamm.com>
2022-01-05 23:29:07 +0100

    added initial code and examples

--- betapoisson.py
+++ betapoisson.py
@@ -3,14 +3,13 @@ from typing import Optional
 from scipy.stats import rv_discrete
 from scipy.special import gamma, hyp1f1, gammaln
 from scipy.optimize import differential_evolution
+from typing import List, Tuple
 import numpy as np
 from matplotlib import pyplot as plt
 
 class BetaPoisson(rv_discrete):
     "Beta-Poisson mixture distribution - a bimodal distribution for modelling zero inflated or jittered distributions."
-    #def __init__(self, y: Optional[np.ndarray[int]] = None):
-    #    self.y = y
-    def _pmf(self, x, mu_max, p, q):
+    def _pmf(self, x: List[int], mu_max:float, p:float, q:float):
         self.mu_max = mu_max
         self.p = p
         self.q = q
@@ -30,8 +29,10 @@ class BetaPoisson(rv_discrete):
 
     def _std(self):
         pass
+
     def _var(self):
         pass
+
     def loglikelihood(self, x, mu_max, p, q):
         n = len(x)
         log_x_factorial = gammaln(x+1)
@@ -39,26 +40,47 @@ class BetaPoisson(rv_discrete):
         return n*(gammaln(p+q) - mu_max - gammaln(p)) + np.log(mu_max)*np.sum(x) + np.sum(gammaln(p+x)) +\
             np.sum(np.log(hyp1f1(q, p+q+x, mu_max)) - log_x_factorial - gammaln(p+q+x))
 
-    def to_minimize(self, params):
+    @staticmethod
+    def _to_minimize(params, y):
         m, p = params
-        return -BetaPoisson().loglikelihood(self.y, m, p, q=.5)
+        return -BetaPoisson().loglikelihood(y, m, p, q=.5)
 
-    def fit(self):
-        pass
+    @classmethod
+    def fit(cls, y:List, bounds:List[Tuple] = None, workers:int = 4):
+        if not bounds:
+            bounds = [
+            [0, 2 * np.amax(y)],
+            # the maximal expectation value for each draw is assumed to be max 2-times the max obs.
+            [0, 4],
+            # [0, 4]
+            ]
+        bp = cls()
+        bp.y = y
+        res = differential_evolution(bp._to_minimize,
+                                     bounds=bounds,
+                                     workers=workers,
+                                     args=(y,))
+        return res
 
 
 if __name__ == "__main__":
-    p = BetaPoisson()
-    y_sim = p.rvs(8.53, .68, .49, size=1000)
-    y = p.pmf(np.arange(20), 8.53, .68, .49)
+    bp = BetaPoisson()
+    mu_max = 8.53
+    p = .68
+    y_sim = bp.rvs(mu_max, p, .5, size=1000)
+    y = bp.pmf(np.arange(20), mu_max, p, .5)
     plt.hist(y_sim, range=[-.5, 100.5], bins=100, density=True)
     plt.plot(y)
     plt.xlim(-.5, 20.5)
-    p.y = y_sim
-    res = differential_evolution(p.to_minimize, bounds=[
+    res = bp.fit(y = y_sim, workers=1)
+    """
+    differential_evolution(p.to_minimize, bounds=[
         [0, 2*np.amax(y_sim)], # the maximal expectation value for each draw is assumed to be max 2-times the max obs.
         [0, 4],
         #[0, 4]
-    ], workers=4)
-    plt.plot(p.pmf(np.arange(20), *res.x, .5))
+    ], workers=4)"""
+    plt.plot(bp.pmf(np.arange(20), *res.x, .5))
     plt.show()
+
+    #import pandas
+    #pandas.read_sas("C:\Users\Johannes.Nagele\Downloads\LLCP2020XPT\LLCP2020.xpt")
